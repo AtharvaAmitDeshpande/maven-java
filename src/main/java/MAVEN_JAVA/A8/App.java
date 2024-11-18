3def7df3f1279a8bcff7025ca563d269c8eb4580
atharva <22bd1a05a8atharva@gmail.com>
2024-11-18 11:10:28 +0530

    maven_java

---
+++ src/main/java/MAVEN_JAVA/A8/App.java
@@ -0,0 +1,14 @@
+package MAVEN_JAVA.A8;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a=5;
+        int b=5;
+        int c= a+b;
+        System.out.println("Sum of two numbers is: "+ c);
+        }
+}
